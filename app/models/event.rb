8ddadece645bd113c7045d367a2eae44fd73fe2c
Ayumi-Imaizumi <walk_key@hotmail.com>
2017-10-28 16:26:31 +0900

    イベント追加時のエラー回避

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,10 +7,14 @@ class Event < ActiveRecord::Base
   has_many :actor_events
 
   def to_s
-    "#{title.name}(#{play_at.strftime("%Y年#{play_at.month}月%d日（#{%w(日 月 火 水 木 金 土)[play_at.wday]}）")}@#{theater.name})"
+    if title && theater && play_at
+      "#{title.name}(#{play_at.strftime("%Y年#{play_at.month}月%d日（#{%w(日 月 火 水 木 金 土)[play_at.wday]}）")}@#{theater.name})"
+    end
   end
 
   def schedule_name
-    "#{title.name}(#{play_at.strftime("%Y年#{play_at.month}月%d日%H:%M（#{%w(日 月 火 水 木 金 土)[play_at.wday]}）")}@#{theater.name})"
+    if title && theater && play_at
+      "#{title.name}(#{play_at.strftime("%Y年#{play_at.month}月%d日%H:%M（#{%w(日 月 火 水 木 金 土)[play_at.wday]}）")}@#{theater.name})"
+    end
   end
 end
